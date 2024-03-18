1abf06ab7518351e1165847064cea9d39782fef5
Salvi-Matteo <153504143+Salvi-Matteo@users.noreply.github.com>
2024-03-18 18:21:41 +0100

    Update preprocess_run.py with the computation of all the 4 folder in one step

--- code_to_run/preprocess_run.py
+++ code_to_run/preprocess_run.py
@@ -1,5 +1,42 @@
 import subprocess
 
+#MR TRAIN
+subprocess.call([  
+    "python", "data/data2tfrecords.py", "--folder" ,"/content/drive/MyDrive/Dataset/mr_train",
+    "--modality", "mr",
+    "--size" ,"128" ,"128", "128" , # image dimension for training
+    "--folder_postfix" ,"_train", # _train or _val, i.e. will process the images/segmentation in ct_train and ct_train_seg
+    "--deci_rate" ,"0" , # decimation rate on ground truth surface meshes
+    "--smooth_ite","50" , # Laplacian smoothing on ground truth surface meshes
+    "--out_folder" , "/content/drive/MyDrive/Processed_dataset" ,
+    "--seg_id", "1" ,"2", "3" ,"4", "5" ,"6" ,"7" # segmentation ids, 1-7 for seven cardiac structures
+])
+
+#MR VAL
+subprocess.call([  
+    "python", "data/data2tfrecords.py", "--folder" ,"/content/drive/MyDrive/Dataset/mr_val",
+    "--modality", "mr",
+    "--size" ,"128" ,"128", "128" , # image dimension for training
+    "--folder_postfix" ,"_val", # _train or _val, i.e. will process the images/segmentation in ct_train and ct_train_seg
+    "--deci_rate" ,"0" , # decimation rate on ground truth surface meshes
+    "--smooth_ite","50" , # Laplacian smoothing on ground truth surface meshes
+    "--out_folder" , "/content/drive/MyDrive/Processed_dataset" ,
+    "--seg_id", "1" ,"2", "3" ,"4", "5" ,"6" ,"7" # segmentation ids, 1-7 for seven cardiac structures
+])
+
+#CT TRAIN
+subprocess.call([  
+    "python", "data/data2tfrecords.py", "--folder" ,"/content/drive/MyDrive/Dataset/ct_train",
+    "--modality", "ct",
+    "--size" ,"128" ,"128", "128" , # image dimension for training
+    "--folder_postfix" ,"_train", # _train or _val, i.e. will process the images/segmentation in ct_train and ct_train_seg
+    "--deci_rate" ,"0" , # decimation rate on ground truth surface meshes
+    "--smooth_ite","50" , # Laplacian smoothing on ground truth surface meshes
+    "--out_folder" , "/content/drive/MyDrive/Processed_dataset" ,
+    "--seg_id", "1" ,"2", "3" ,"4", "5" ,"6" ,"7" # segmentation ids, 1-7 for seven cardiac structures
+])
+
+#CT VAL
 subprocess.call([  
     "python", "data/data2tfrecords.py", "--folder" ,"/content/drive/MyDrive/Dataset/ct_val",
     "--modality", "ct",
