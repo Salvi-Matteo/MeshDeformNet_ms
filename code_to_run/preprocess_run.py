7f1398b786260137235701e1c263e8c5a89f5c1f
Salvi-Matteo <153504143+Salvi-Matteo@users.noreply.github.com>
2024-03-18 18:06:00 +0100

    Update preprocess_run.py with real paths

--- code_to_run/preprocess_run.py
+++ code_to_run/preprocess_run.py
@@ -1,13 +1,12 @@
 import subprocess
 
-subprocess.call([   
-"python",    "data/data2tfrecords.py",
-    "--folder",    "/path/to/top/image/directory",
-    "--modality",    "ct",    "mr",    
-    "--size",    "128",    "128",    "128",
-    "--folder_postfix",    "_train",
-    "--deci_rate",    "0",
-    "--smooth_ite",    "50",
-    "--out_folder",    "/path/to/output",
-    "--seg_id",    "1",    "2",    "3",    "4",    "5",    "6",    "7"
+subprocess.call([  
+    "python", "data/data2tfrecords.py", "--folder" ,"/content/drive/MyDrive/Dataset/ct_val",
+    "--modality", "ct",
+    "--size" ,"128" ,"128", "128" , # image dimension for training
+    "--folder_postfix" ,"_val", # _train or _val, i.e. will process the images/segmentation in ct_train and ct_train_seg
+    "--deci_rate" ,"0" , # decimation rate on ground truth surface meshes
+    "--smooth_ite","50" , # Laplacian smoothing on ground truth surface meshes
+    "--out_folder" , "/content/drive/MyDrive/Processed_dataset" ,
+    "--seg_id", "1" ,"2", "3" ,"4", "5" ,"6" ,"7" # segmentation ids, 1-7 for seven cardiac structures
 ])
